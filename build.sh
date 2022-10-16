8bcebf9b46d802149f5522fede0dfe4d2c48bcf6
Maniero <carlosmaniero@gmail.com>
2022-10-15 23:35:27 -0300

    cleanup

--- build.sh
+++ build.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/env sh
 
-repeat(){
-  for i in {1..79}; do echo -n "$1"; done
-}
-
-printLine () {
-    echo -n "+"
-    repeat "-"
-    echo
-}
-
-logStep () {
-    printLine
-    echo "| $1"
-    printLine
-}
-
-logStep "Generating build files..."
 cmake -S . -B build
-
-logStep "Building..."
 cmake --build build
 
 cp ./build/mc ./bin/mc-llvm
--- setup.sh
+++ setup.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env sh
 
-repeat(){
-  for i in {1..79}; do echo -n "$1"; done
-}
-
-printLine () {
-    echo -n "+"
-    repeat "-"
-    echo
-}
-
-logStep () {
-    printLine
-    echo "| $1"
-    printLine
-}
-
 sh ./build.sh
 
 python3 -m venv build/venv
